9a68507c5104895189dc0fbcc4d72b4a1215ef59
Juan Medina <juanmedi730@gmail.com>
2025-07-16 21:56:07 -0500

    creación del proyecto, estructura y componentes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-red">DashBoard</h1>
+    </div>
+  );
+}
+
+export default App;
